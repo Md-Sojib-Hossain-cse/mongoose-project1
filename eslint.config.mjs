211b67e2bbb164871a2e2a9e5a39b88f82494ed2
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2024-11-20 23:43:53 +0600

    create student feature

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,28 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintPluginPrettierRecommended from 'eslint-config-prettier';
 
-const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
-
+// const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended')
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   eslintPluginPrettierRecommended,
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
   { languageOptions: { globals: globals.node } },
   {
-    ignores: ["node_modules", "dist"],
+    ignores: ['node_modules', 'dist'],
     rules: {
-      "no-unused-vars": "error",
-      "no-unused-expressions": "error",
-      "prefer-const":"error",
-      "no-console":"warn",
-      "no-undef":"error"
+      'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
+      'no-undef': 'error',
     },
-    "globals":{
-      "process":"readonly"
-    }
+  },
+  {
+    ignores: ['node_modules/*', '.env'],
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-];
+]
---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from 'express';
+import { StudentServices } from './student.service';
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const { student: studentData } = req.body;
+
+    //will call service function to send this data
+    const result = await StudentServices.createStudentIntoDB(studentData);
+
+    //send response
+    res.status(200).json({
+      success: true,
+      message: 'Student created successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDB();
+    res.status(200).json({
+      success: true,
+      message: 'Student data retrieve successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  const { studentId } = req.params;
+
+  const result = await StudentServices.getSingleStudentById(studentId);
+
+  res.status(200).json({
+    success: true,
+    message: 'Single student data retrieve successfully',
+    data: result,
+  });
+};
+
+export const studentController = {
+  createStudent,
+  getAllStudents,
+  getSingleStudent,
+};
